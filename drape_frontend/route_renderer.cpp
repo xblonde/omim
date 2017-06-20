17bd8915321dc96c1d599d7511478eaf63c78df3
r.kuznetsov <r.kuznetsov@corp.mail.ru>
2017-06-20 16:31:44 +0300

    Added time label rendering

--- drape_frontend/route_renderer.cpp
+++ drape_frontend/route_renderer.cpp
@@ -43,8 +43,6 @@ std::vector<float> const kHalfWidthInPixelOthers =
   1.5f, 1.5f, 2.0f, 2.5f, 3.0, 4.0f, 5.0f, 5.0f, 9.0f, 13.0f
 };
 
-
-
 std::vector<float> const kPreviewPointRadiusInPixel =
 {
   // 1   2     3     4     5     6     7     8     9     10
@@ -68,8 +66,12 @@ void InterpolateByZoom(drape_ptr<Subroute> const & subroute, ScreenBase const & 
   ExtractZoomFactors(screen, zoom, index, lerpCoef);
 
   std::vector<float> const * halfWidthInPixel = &kHalfWidthInPixelOthers;
-  if (subroute->m_routeType == RouteType::Car || subroute->m_routeType == RouteType::Taxi)
+  if (subroute->m_routeType == RouteType::Car ||
+      subroute->m_routeType == RouteType::Taxi ||
+      subroute->m_routeType == RouteType::Subway)
+  {
     halfWidthInPixel = &kHalfWidthInPixelCar;
+  }
 
   halfWidth = InterpolateByZoomLevels(index, lerpCoef, *halfWidthInPixel);
   halfWidth *= static_cast<float>(df::VisualParams::Instance().GetVisualScale());
